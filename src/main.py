57a2745ed5bb27e6b56bb9f28c5a3d3e94f321c2
erick_coutinho <erickcoutinho@alu.ufc.br>
2024-09-02 16:10:26 -0300

    Santander - Não precisou de modificação, usando o leitor padrao (igual do inter)

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-import tkinter as tk
-from tkinter import ttk
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
 from tkinter import messagebox
 from tkinter import filedialog
-from processador_ofx import processar_arquivo_ofx, remove_closing_tags
+from processador_ofx_geral import processar_arquivo_ofx, remove_closing_tags
 from extract_PagBank import extract_ofx_data
 import ofxparse
 import io
@@ -17,23 +17,23 @@ def processar_ofx():
 
     try:
         if banco_selecionado == "Bradesco":
-            # Leitura específica para o arquivo Bradesco
             with open(caminho_arquivo, 'r', encoding='utf-8', errors="ignore") as file:
                 decoded_content = file.read()
                 modified_content = remove_closing_tags(decoded_content)
                 cleaned_file = io.StringIO(modified_content)
                 ofx = ofxparse.OfxParser.parse(cleaned_file)
             processar_arquivo_ofx(ofx)
+        elif banco_selecionado == "Santander":
+            with open(caminho_arquivo, 'r', encoding='utf-8') as file:
+                ofx = ofxparse.OfxParser.parse(file)
+            processar_arquivo_ofx(ofx)
         elif banco_selecionado == "Inter":
-            # Leitura específica para o arquivo Inter
             with open(caminho_arquivo, 'r', encoding='utf-8') as file:
                 ofx = ofxparse.OfxParser.parse(file)
             processar_arquivo_ofx(ofx)
         elif banco_selecionado == "PagBank":
-            # Leitura e processamento específico para o banco PagBank
             transactions_df, agency, account_number, account_type, initial_balance, available_balance, start_date, end_date = extract_ofx_data(caminho_arquivo)
 
-            # Criando um objeto simulado com a estrutura necessária
             class AccountStatement:
                 def __init__(self, transactions, start_date, end_date):
                     self.start_date = start_date
@@ -51,7 +51,6 @@ def processar_ofx():
                 def __init__(self, account):
                     self.account = account
 
-            # Convertendo transações para o formato esperado
             transactions = []
             for _, row in transactions_df.iterrows():
                 transaction = ofxparse.Transaction()
@@ -61,12 +60,10 @@ def processar_ofx():
                 transaction.id = row['ID da Transação']
                 transactions.append(transaction)
 
-            # Criando o objeto de conta e extrato
             statement = AccountStatement(transactions, start_date, end_date)
             account = Account(agency, account_number, account_type, statement)
             ofx_data = OFXData(account)
 
-            # Chama o método para processar e gerar o Excel
             processar_arquivo_ofx(ofx_data)
 
             messagebox.showinfo("Sucesso", f"Arquivo OFX processado com sucesso para {banco_selecionado}.\nAgência: {agency}\nConta: {account_number}")
@@ -79,25 +76,27 @@ def processar_ofx():
     messagebox.showinfo("Sucesso", "Arquivo OFX processado e Excel criado com sucesso.")
 
 # Interface
-root = tk.Tk()
+root = ttk.Window(themename="darkly")
 root.title("Processador de Arquivos OFX")
 
 # Variável para armazenar a escolha do usuário
-banco_var = tk.StringVar(value="Inter")
+banco_var = ttk.StringVar(value="Inter")
 
-label_instrucoes = tk.Label(root, text="Selecione o banco correspondente ao arquivo OFX:")
+label_instrucoes = ttk.Label(root, text="Selecione o banco correspondente ao arquivo OFX:")
 label_instrucoes.pack(pady=10)
 
 # Opções de banco (Bradesco, Inter, PagBank)
-radio_bradesco = ttk.Radiobutton(root, text="Bradesco", variable=banco_var, value="Bradesco")
-radio_inter = ttk.Radiobutton(root, text="Inter", variable=banco_var, value="Inter")
-radio_pagbank = ttk.Radiobutton(root, text="PagBank", variable=banco_var, value="PagBank")
+radio_bradesco = ttk.Radiobutton(root, text="Bradesco", variable=banco_var, value="Bradesco", bootstyle="info")
+radio_inter = ttk.Radiobutton(root, text="Inter", variable=banco_var, value="Inter", bootstyle="info")
+radio_pagbank = ttk.Radiobutton(root, text="PagBank", variable=banco_var, value="PagBank", bootstyle="info")
+radio_santander = ttk.Radiobutton(root, text="Santander", variable=banco_var, value="Santander", bootstyle="info")
 
-radio_bradesco.pack(anchor=tk.W)
-radio_inter.pack(anchor=tk.W)
-radio_pagbank.pack(anchor=tk.W)
+radio_bradesco.pack(anchor=ttk.W)
+radio_inter.pack(anchor=ttk.W)
+radio_pagbank.pack(anchor=ttk.W)
+radio_santander.pack(anchor=ttk.W)
 
-botao_processar = ttk.Button(root, text="Selecionar Arquivo e Processar", command=processar_ofx)
+botao_processar = ttk.Button(root, text="Selecionar Arquivo e Processar", command=processar_ofx, bootstyle="primary")
 botao_processar.pack(pady=20)
 
 root.mainloop()
